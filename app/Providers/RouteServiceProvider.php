8348ed63095a38b9e10f4af56fb71947fcba7144
evisa-backend <johnsonsemmanuel@gmail.com>
2026-07-21 17:48:35 +0000

    fix: add admin URL prefix to routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -52,7 +52,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
-            Route::middleware('web')
+            Route::prefix('admin')
+                ->middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/admin.php'));
 
